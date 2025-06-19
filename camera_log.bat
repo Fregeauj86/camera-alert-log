868693d62144aa9cd6bde7ac05a594f28dfa6746
Fregeauj86 <fregeauj86@gmail.com>
2025-06-20 00:49:42 +0700

    Update camera_log.bat

--- camera_log.bat
+++ camera_log.bat
@@ -1,6 +1,6 @@
 @echo off
 :loop
-ping 192.168.1.8 -n 1 >nul
+ping 192.168.1.X -n 1 >nul
 if errorlevel 1 (
     echo [%date% %time%] Camera is OFFLINE >> camera_log.txt
     echo Camera is offline!
